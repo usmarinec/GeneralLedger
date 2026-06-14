4230e8d4c3d54ca340907022afe36e9caa523b65
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-14 03:53:50 +0000

    activated test profile to use h2 database for test runtime only

--- ledger-backend/src/test/java/com/usmarinec/ledger/LedgerApplicationTests.java
+++ ledger-backend/src/test/java/com/usmarinec/ledger/LedgerApplicationTests.java
@@ -2,12 +2,12 @@ package com.usmarinec.ledger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LedgerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
