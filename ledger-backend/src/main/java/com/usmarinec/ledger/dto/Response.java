16957b053ff7cac844beab30178464cd8952e794
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-17 19:11:45 +0000

    Created base dto record interfaces

---
+++ ledger-backend/src/main/java/com/usmarinec/ledger/dto/Response.java
@@ -0,0 +1,7 @@
+package com.usmarinec.ledger.dto;
+
+import java.util.UUID;
+
+public interface Response {
+    UUID id();
+}
---
+++ ledger-backend/src/main/java/com/usmarinec/ledger/dto/UpdateRequest.java
@@ -0,0 +1,5 @@
+package com.usmarinec.ledger.dto;
+
+public interface UpdateRequest {
+    
+}
