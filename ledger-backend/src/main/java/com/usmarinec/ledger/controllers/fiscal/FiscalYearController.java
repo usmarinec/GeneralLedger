134bcba461e6a850f6dd67b275f143311a687b79
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-18 19:43:40 +0000

    added endpoint for FiscalYearController

--- ledger-backend/src/main/java/com/usmarinec/ledger/controllers/fiscal/FiscalYearController.java
+++ ledger-backend/src/main/java/com/usmarinec/ledger/controllers/fiscal/FiscalYearController.java
@@ -6,7 +6,15 @@ import com.usmarinec.ledger.dto.fiscal.CreateFiscalYearRequest;
 import com.usmarinec.ledger.dto.fiscal.FiscalYearResponse;
 import com.usmarinec.ledger.dto.fiscal.UpdateFiscalYearRequest;
 import com.usmarinec.ledger.repositories.fiscal.FiscalYearRepository;
+import com.usmarinec.ledger.responses.SuccessFailureResponse;
+import com.usmarinec.ledger.responses.SuccessFailureResponseUtility;
 import com.usmarinec.ledger.services.fiscal.FiscalYearService;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +27,20 @@ public class FiscalYearController
         CreateFiscalYearRequest,
         UpdateFiscalYearRequest,
         FiscalYearResponse,
-        FiscalYearService> {}
+        FiscalYearService> {
+
+  /**
+   * Fetches all FiscalYears by AccountingEntity ID.
+   *
+   * @param accountingEntityId id to search
+   * @return SuccessFailureResponse with List of FiscalYears
+   */
+  @GetMapping("/fetchByAcctEntity/{accountingEntityId}")
+  public ResponseEntity<SuccessFailureResponse<FiscalYearResponse>> fetchByAccountingEntityId(
+      @PathVariable UUID accountingEntityId) {
+    List<FiscalYearResponse> fiscalYearsList =
+        this.service.findByAccountingEntity(accountingEntityId);
+    return SuccessFailureResponseUtility.createSuccessFailureResponse(
+        true, "Record(s) found", HttpStatus.OK, fiscalYearsList);
+  }
+}
