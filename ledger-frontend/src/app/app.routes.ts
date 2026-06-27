fd923d493fe03f9a6500d0de8d49914aab0799cd
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-27 04:29:18 +0000

    updated app routes

--- ledger-frontend/src/app/app.routes.ts
+++ ledger-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from "@angular/router";
+import { LedgerWorkspaceComponent } from './features/ledger-workspace/ledger-workspace'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LedgerWorkspaceComponent
+    }    
+];
