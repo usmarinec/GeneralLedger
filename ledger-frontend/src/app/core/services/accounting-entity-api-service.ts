9645d7e153730989810c4e1e35dfc56b83792523
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-27 02:18:11 +0000

    created accounting entity api service

---
+++ ledger-frontend/src/app/core/services/accounting-entity-api-service.ts
@@ -0,0 +1,19 @@
+// src/app/core/services/accounting-entity-api.ts
+
+import { Service } from '@angular/core';
+
+import { CrudApi } from '../api/crud-api';
+import {
+  AccountingEntityResponse,
+  CreateAccountingEntityRequest,
+  UpdateAccountingEntityRequest
+} from '../../shared/models/accounting-entity.model';
+
+@Service()
+export class AccountingEntityApiService extends CrudApi<
+  AccountingEntityResponse,
+  CreateAccountingEntityRequest,
+  UpdateAccountingEntityRequest
+> {
+  protected override readonly baseUrl = '/api/accounting-entities';
+}
