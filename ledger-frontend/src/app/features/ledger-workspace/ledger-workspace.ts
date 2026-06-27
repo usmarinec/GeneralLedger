67f17a87672ef3dfd82635093b6e8e2d2d6a5ac7
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-27 04:19:56 +0000

    generated ledger workspace component

---
+++ ledger-frontend/src/app/features/ledger-workspace/ledger-workspace.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-ledger-workspace",
+  imports: [],
+  templateUrl: "./ledger-workspace.html",
+  styleUrl: "./ledger-workspace.scss",
+})
+export class LedgerWorkspace {}
