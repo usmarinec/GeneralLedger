ebafef6133e4ab706e5ea0386f23575dc58ca965
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-27 04:31:04 +0000

    spotless

--- ledger-frontend/src/app/app.routes.ts
+++ ledger-frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from "@angular/router";
-import { LedgerWorkspaceComponent } from './features/ledger-workspace/ledger-workspace'
+import { LedgerWorkspaceComponent } from "./features/ledger-workspace/ledger-workspace";
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LedgerWorkspaceComponent
-    }    
+  {
+    path: "",
+    component: LedgerWorkspaceComponent,
+  },
 ];
