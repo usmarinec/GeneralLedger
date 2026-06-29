a380e51f4eed510dc2545e7610250e6a616b37dd
EC2 Default User <ec2-user@ip-82-0-0-109.us-west-1.compute.internal>
2026-06-29 00:00:13 +0000

    moved context selector to app shell and removed from individual components

--- ledger-frontend/src/app/layout/app-shell/app-shell.ts
+++ ledger-frontend/src/app/layout/app-shell/app-shell.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from "@angular/core";
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
+import { LedgerContextSelector } from "../../shared/components/ledger-context-selector/ledger-context-selector";
+
 @Component({
   selector: "app-shell",
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, LedgerContextSelector],
   templateUrl: "./app-shell.html",
   styleUrl: "./app-shell.scss",
 })
 export class AppShell {
   private static readonly SIDEBAR_COLLAPSED_KEY = "ledger.sidebarCollapsed";
 
-  readonly sidebarCollapsed = signal<boolean>(
+  readonly sidebarCollapsed = signal(
     localStorage.getItem(AppShell.SIDEBAR_COLLAPSED_KEY) === "true"
   );
 
